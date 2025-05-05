d2c9b4ac35e44eae43a74ead24c21d93652c469d
Roman Stryzhka <rstrizhko1994@gmail.com>
2025-05-05 16:29:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import IButton from './components/Ibutton/IButton.vue'
+
+const classes = {
+  'bg-black': false,
+  'bg-red': true,
+  'btn': true,
+}
+</script>
+
+<template>Hello vue <IButton :class="classes"></IButton></template>
+
+<style>
+.btn {
+  color: white;
+}
+
+.bg-black {
+  background-color: black;
+}
+
+.bg-red {
+  background-color: red;
+}
+</style>
